f1887168d11a6d552f364dec5d118f21076985b2
Mary Xia <maryxia@google.com>
2017-10-13 16:52:24 -0700

    in process of adding textarea

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -10,9 +10,6 @@ class Todo extends StatelessWidget {
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Todo',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
       home: new TodoList(),
     );
   }
@@ -26,28 +23,256 @@ class TodoList extends StatefulWidget {
 }
 
 class _TodoState extends State<TodoList> {
-//  var _items = <String>[
-//    'Clean the room',
-//    'Feed the rabbit',
-//    'This is a really long to do item. I guess it can be truncated. Or it does not have to be. Continue typing.',
-//    'Mail rent check',
-//    'Buy groceries',
-//    'Sign up for that pottery class'
-//  ];
-//
+  final _items = <String>[
+    'Clean the room',
+    'Feed the rabbit',
+    'This is a really long to do item. I guess it can be truncated. Or it does not have to be. Continue typing.',
+    'Mail rent check',
+    'Buy groceries',
+    'Sign up for that pottery class',
+    'Clean the room',
+    'Feed the rabbit',
+    'This is a really long to do item. I guess it can be truncated. Or it does not have to be. Continue typing.',
+    'Mail rent check',
+    'Buy groceries',
+    'Sign up for that pottery class',
+    'Clean the room',
+    'Feed the rabbit',
+    'This is a really long to do item. I guess it can be truncated. Or it does not have to be. Continue typing.',
+    'Mail rent check',
+    'Buy groceries',
+    'Sign up for that pottery class',
+    'Clean the room',
+    'Feed the rabbit',
+    'This is a really long to do item. I guess it can be truncated. Or it does not have to be. Continue typing.',
+    'Mail rent check',
+    'Buy groceries',
+    'Sign up for that pottery class',
+  ];
+  final _done = <String>[
+    'Mow the lawn',
+    'Bake some croissants',
+    'Clean the room',
+    'Feed the rabbit',
+    'This is a really long to do item. I guess it can be truncated. Or it does not have to be. Continue typing.',
+    'Mail rent check',
+    'Buy groceries',
+    'Sign up for that pottery class',
+    'Clean the room',
+    'Feed the rabbit',
+    'This is a really long to do item. I guess it can be truncated. Or it does not have to be. Continue typing.',
+    'Mail rent check',
+    'Buy groceries',
+    'Sign up for that pottery class',
+  ];
+  var _currentPage;
+  var _todoPage;
+  var _donePage;
+  bool _userIsTyping = false;
 
-  var _items = <String>[];
-  var _listItems = <Widget>[];
+  void _chooseList(int value) {
+    switch (value) {
+      case 0:
+        if (_currentPage != _todoPage) {
+          setState(() {
+            _currentPage = _todoPage;
+          });
+        }
+        break;
+      case 1:
+        if (_currentPage != _donePage) {
+          setState(() {
+            _currentPage = _donePage;
+          });
+        }
+        break;
+    }
+  }
 
-  //for (var i in _items) {
-//    _listItems.add(new Text(item));
-//  }
+  void _showSaveOptions(String value) {
+    setState(() {
+      _userIsTyping = true;
+    });
+  }
+
+  void _buildItems() {
+    var _todoItems = <Widget>[];
+    var _doneItems = <Widget>[];
+    _todoItems.add(new Container(
+      color: Colors.amber[100],
+      padding: const EdgeInsets.all(16.0),
+      child: new Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          new Flexible(
+            fit: FlexFit.loose,
+            child: new Container(
+              alignment: FractionalOffset.topLeft,
+              child: new TextFormField(
+                style: new TextStyle(
+                  color: Colors.grey,
+                  fontSize: 50.0,
+                ),
+                decoration: new InputDecoration(
+                  hintText: 'Add a note',
+                  hideDivider: true,
+                  hintStyle: new TextStyle(
+                    color: Colors.grey,
+                    fontSize: 30.0, // Throws an error if you don't specify
+                  ),
+                ),
+                maxLines: 2,
+                //onChanged: (value) => _showSaveOptions(value),
+              ),
+            ),
+          ),
+          new Row(
+            children: <Widget>[
+              new Icon(
+                Icons.attach_file,
+                size: 50.0,
+              ),
+              new Offstage(
+                offstage: !_userIsTyping,
+                child: new Icon(
+                  Icons.check,
+                  size: 50.0,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    ));
+
+    for (var i in _items) {
+      _todoItems.add(new Container(
+          color: Colors.green[50],
+          margin: const EdgeInsets.only(
+            left: 8.0,
+            right: 8.0,
+            top: 8.0,
+          ),
+          padding: const EdgeInsets.all(16.0),
+          child: new Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              new Flexible(
+                fit: FlexFit.loose,
+                child: new Container(
+                  child: new Text(
+                    i,
+                    softWrap: true,
+                    style: new TextStyle(
+                      color: Colors.grey[800],
+                      fontSize: 30.0,
+                    ),
+                  ),
+                ),
+              ),
+              new Row(
+                children: <Widget>[
+                  new Icon(Icons.check),
+                  new Icon(Icons.edit),
+                  new Icon(Icons.delete_forever),
+                ],
+              ),
+            ],
+          ),
+      ));
+    }
+
+    for (var i in _done) {
+      _doneItems.add(new Container(
+          color: Colors.grey[100],
+          margin: const EdgeInsets.only(
+            left: 8.0,
+            right: 8.0,
+            top: 8.0,
+          ),
+          padding: const EdgeInsets.all(16.0),
+          child: new Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              new Flexible(
+                fit: FlexFit.loose,
+                child: new Container(
+                  child: new Text(
+                    i,
+                    softWrap: true,
+                    style: new TextStyle(
+                      color: Colors.grey[500],
+                      fontSize: 30.0,
+                    ),
+                  ),
+                ),
+              ),
+              new Row(
+                children: <Widget>[
+                  new Icon(Icons.undo),
+                  new Icon(Icons.delete_forever),
+                ],
+              ),
+            ],
+          ),
+      ));
+    }
+
+    setState(() {
+      _todoPage = new ListView.builder(
+        itemBuilder: (BuildContext context, int index) => _todoItems[index],
+        itemCount: _todoItems.length,
+      );
+      _donePage = new ListView.builder(
+        itemBuilder: (BuildContext context, int index) => _doneItems[index],
+        itemCount: _doneItems.length,
+      );
+
+      _currentPage = _todoPage;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return new ListView.builder(
-      itemBuilder: (BuildContext context, int index) => _listItems[index],
-      itemCount: _listItems.length,
+    if (_todoPage == null) {
+      _buildItems();
+    }
+
+    var navBarItems = <BottomNavigationBarItem>[
+      new BottomNavigationBarItem(
+        icon: new Icon(Icons.event_note),
+        title: new Text('Todo'),
+        backgroundColor: Colors.orange,
+      ),
+      new BottomNavigationBarItem(
+        icon: new Icon(Icons.done_all),
+        title: new Text('Done'),
+        backgroundColor: Colors.purple,
+      ),
+    ];
+
+    return new Theme(
+      data: new ThemeData(
+        canvasColor: Colors.blueGrey[50],
+      ),
+      // TODO(maryx) remove this stack
+      child: new Stack(
+        children: <Widget>[
+          new Scaffold(
+            body: _currentPage,
+            bottomNavigationBar: new BottomNavigationBar(
+              items: navBarItems,
+              fixedColor: Colors.green,
+              onTap: (value) => _chooseList(value),
+              type: BottomNavigationBarType.fixed,
+            ),
+            resizeToAvoidBottomPadding: false,
+          ),
+        ],
+      ),
     );
   }
 }
