3521676bdb5e10ae919d9cece121c32c3ff3f3b4
Collin Jackson <jackson@google.com>
2017-11-15 13:17:41 -0800

    Wire up SliverAppBar

--- todo/lib/main.dart
+++ todo/lib/main.dart
@@ -2,32 +2,64 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 void main() {
-  runApp(new Todo());
+  runApp(new TodoApp());
 }
 
-class Todo extends StatelessWidget {
+class TodoApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Todo',
-      home:  new Scaffold(
-        appBar: new AppBar(
-          title: new Text("ToDo")
-        ),
-        body: new StreamBuilder(
-          stream: Firestore.instance.collection('tasks').snapshots,
-          builder: (context, snapshot) {
-            if (!snapshot.hasData) return new Text('Loading...');
-            return new ListView(
-              children: snapshot.data.documents.map((document) {
-                return new ListTile(
-                  title: new Text(document['title']),
-                );
-              }).toList(),
-            );
-          },
-        ),
+      theme: new ThemeData(
+        primarySwatch: Colors.red,
+      ),
+      home: new TodoHome(),
+    );
+  }
+}
+
+class TodoHome extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: new StreamBuilder(
+        stream: Firestore.instance
+            .collection('tasks')
+            .snapshots,
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return new Text('Loading...');
+          return new CustomScrollView(
+            slivers: [
+              new SliverAppBar(
+                expandedHeight: 200.0,
+                flexibleSpace: new FlexibleSpaceBar(
+                  title: new Text('todo'),
+                  centerTitle: true,
+                  background: new Image.asset(
+                    'assets/notebook.jpg',
+                    color: Theme
+                        .of(context)
+                        .primaryColor,
+                    colorBlendMode: BlendMode.screen,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+              new SliverList(
+                delegate: new SliverChildBuilderDelegate(
+                      (BuildContext context, int index) {
+                    if (index >= snapshot.data.documents.length)
+                      return null;
+                    return new ListTile(
+                      title: new Text(snapshot.data.documents[index]['title']),
+                    );
+                  },
+                ),
+              ),
+            ],
+          );
+        },
       ),
     );
   }
