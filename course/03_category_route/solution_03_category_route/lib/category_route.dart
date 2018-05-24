ce41a0d24fc0a5ced5c3b490db9586a2a462c081
Gaurav Agrawal <agrawalgaurav1999@gmail.com>
2018-05-24 23:43:31 +0530

    Added Comment for more understanding. (#118)
    * Added Comment for more understanding.
    
    It took me some time to figure out the purpose of this function hence , I tried to share what it's meant for so that others can get it easily.
    
    * Changed the comment using suggestion of @Sfshaza
    
    Thank you for your valuable feedback, I hope it's correct now
    
    * Tried To Implement @maryx sir's recommendation
    
    I tired to update the changes recommended by @maryx . Updated the comment by combining it with previous one and used list instead of Array.
    
    * Updated comment , after review from @maryx
    
    Removed extra space
    
    * comment  update and space removal at line no 46
    
    Updated the comment and removed space suggested by @maryx
    
    * Add back /// for newline

--- course/03_category_route/solution_03_category_route/lib/category_route.dart
+++ course/03_category_route/solution_03_category_route/lib/category_route.dart
@@ -42,7 +42,7 @@ class CategoryRoute extends StatelessWidget {
 
   /// Makes the correct number of rows for the list view.
   ///
-  /// For portrait, we use a [ListView].
+  /// For portrait, we construct a [ListView] from the list of category widgets.
   Widget _buildCategoryWidgets(List<Widget> categories) {
     return ListView.builder(
       itemBuilder: (BuildContext context, int index) => categories[index],
